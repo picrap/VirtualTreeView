4b48cf7e15854a34945ab4c2af69c58532fe9f12
Pascal Craponne <pascal@craponne.fr>
2016-10-24 18:06:54 +0200

    Added projects

---
+++ VirtualTreeView/VirtualTreeView.cs
@@ -0,0 +1,30 @@
+﻿// VirtualTreeView - a TreeView that *actually* allows virtualization
+// https://github.com/picrap/VirtualTreeView
+
+namespace VirtualTreeView
+{
+    using System.Collections.Specialized;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Linq;
+    using System.Windows.Media;
+
+    [StyleTypedProperty(Property = "ItemContainerStyle", StyleTargetType = typeof(TreeViewItem))]
+    public class VirtualTreeView : ItemsControl
+    {
+        static VirtualTreeView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(VirtualTreeView), new FrameworkPropertyMetadata(typeof(ItemsControl)));
+        }
+
+        public VirtualTreeView()
+        {
+            INotifyCollectionChanged notifyCollectionChangedItems = Items;
+            notifyCollectionChangedItems.CollectionChanged += OnItemsCollectionChanged;
+        }
+
+        private void OnItemsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+        }
+    }
+}
