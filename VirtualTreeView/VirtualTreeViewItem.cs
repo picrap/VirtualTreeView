b2d80c5a7778c79403fa1e26cffa6e5842b16f49
Pascal Craponne <pascal@craponne.fr>
2016-10-24 18:13:17 +0200

    Added VirtualTreeViewItem
    But all of this is going to disappear

---
+++ VirtualTreeView/VirtualTreeViewItem.cs
@@ -0,0 +1,10 @@
+﻿// VirtualTreeView - a TreeView that *actually* allows virtualization
+// https://github.com/picrap/VirtualTreeView
+
+namespace VirtualTreeView
+{
+    using System.Windows.Controls;
+    public class VirtualTreeViewItem : HeaderedItemsControl
+    {
+    }
+}
