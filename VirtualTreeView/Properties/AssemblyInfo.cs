0bfeb6c7d0c9177c85ec93fb1f8bead3f5bf4a09
Pascal Craponne <pascal@craponne.fr>
2016-10-26 21:46:57 +0200

    Assembly cleanup

--- VirtualTreeView/Properties/AssemblyInfo.cs
+++ VirtualTreeView/Properties/AssemblyInfo.cs
@@ -1,25 +1,12 @@
-﻿using System.Reflection;
+﻿// VirtualTreeView - a TreeView that *actually* allows virtualization
+// https://github.com/picrap/VirtualTreeView
+
+using System;
+using System.Reflection;
 using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("VirtualTreeView")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VirtualTreeView")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+using System.Windows.Markup;
 
 //In order to begin building localizable applications, set 
 //<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
@@ -31,25 +18,30 @@ using System.Windows;
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 
-[assembly:ThemeInfo(
+[assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                             //(used if a resource is not found in the page, 
-                             // or application resource dictionaries)
+                                     //(used if a resource is not found in the page, 
+                                     // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                      //(used if a resource is not found in the page, 
-                                      // app, or any theme specific resource dictionaries)
+                                              //(used if a resource is not found in the page, 
+                                              // app, or any theme specific resource dictionaries)
 )]
 
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTitle("VirtualTreeView")]
+
+[assembly: AssemblyDescription("A WPF TreeView that actually supports virtualization. It is based on an ItemsControl with VirtualizingStackPanel.")]
+[assembly: AssemblyCompany("openstore.craponne.fr")]
+[assembly: AssemblyProduct("VirtualTreeView")]
+[assembly: AssemblyCopyright("MIT license http://opensource.org/licenses/mit-license.php")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1")]
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: XmlnsDefinition("urn:VirtualTreeView", "VirtualTreeView")]
+
+[assembly: XmlnsPrefix("urn:VirtualTreeView", "vtv")]
