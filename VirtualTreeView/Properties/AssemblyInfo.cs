01c74222dc261fe6c77512c657e2c4e1bff3c6e4
Pascal Craponne <pascal@craponne.fr>
2016-10-27 18:29:11 +0200

    Release 0.2

--- VirtualTreeView/Properties/AssemblyInfo.cs
+++ VirtualTreeView/Properties/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using System.Windows.Markup;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyVersion("0.2")]
 
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
